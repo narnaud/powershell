fd1ffb800ddf09a67abed88cad13922c284c8aa3
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2018-09-26 22:31:31 +0200

    Clean the profile file, add comments

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
-#User variables
+# User variables
+# ==============
 $ToolsFile = "C:\nicolas\Config\tools\tools.json"
 $HomeDir = "C:\nicolas"
 $AliasHash = @{
@@ -14,36 +15,16 @@ $AliasHash = @{
 	tools = 'start "C:\\Program Files\\Notepad++\\notepad++.exe" $ToolsFile'
 }
 
-# PSReadline customization
-# Set-PSReadlineKeyHandler -Key Tab -Function Complete # Unix completion
-Set-PSReadlineOption -BellStyle None # Remove the bell
 
-# Make sure to replace && with ; when copying git commands
-Set-PSReadlineKeyHandler -Key Ctrl+V `
-                         -BriefDescription SpecialGerritCopy `
-                         -LongDescription "Replace && with ; in git specific command line" `
-                         -ScriptBlock {
-    $text = Get-Clipboard
-
-    if ($text -ne $null) {
-        # check if it's a git command
-        if ($text -like "git*") {
-            [Microsoft.PowerShell.PSConsoleReadLine]::Insert($text.replace("&&", ";"))
-        } else {
-            [Microsoft.PowerShell.PSConsoleReadLine]::Insert($text)
-        }
-    }
-}
-
-#Set-Location "~"
+# Home location
+# ==============
+# Change the ~ location - $env:HOME is still C:\Users\nicolas
+(Get-PsProvider 'FileSystem').home = $HomeDir
 Set-Location $HomeDir
 
-# Chocolatey profile
-#$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-#if (Test-Path($ChocolateyProfile)) {
-#  Import-Module "$ChocolateyProfile"
-#}
 
+# Utility methods
+# ===============
 # Start a new powershell window in a specific directory
 function Start-NewWindow([String]$Path)
 {
@@ -51,27 +32,7 @@ function Start-NewWindow([String]$Path)
     cmd $cline
 }
 
-# Posh-Alias module
-Import-Module posh-alias
-$AliasHash.GetEnumerator() | ForEach-Object{ Add-Alias $_.key $_.value }
-
-# Ps-Env
-Import-Module ps-env
-Set-PsEnvConfig $ToolsFile
-
-# Posh-Git
-Import-Module posh-git
-Start-SshAgent -Quiet
-
-# Cd-extra
-Import-Module cd-extras
-
-# Change the ~ location - $env:HOME is still C:\Users\nicolas
-# Usefull for cd without arguments
-(Get-PsProvider 'FileSystem').home = 'C:\nicolas'
-
-# Prompt improvement
-# Rewrite the path so it displays only 2 items, and '~' if it's the Filesystem hom (may be different from $home)
+# Returns a path with only 2 items, and ~ used for $HomeDir
 function Get-EnhancedPromptPath {
 	#$HomeDir = (Get-PsProvider 'FileSystem').home
 	if($pwd -like $HomeDir) { return '~' }
@@ -97,6 +58,58 @@ function Get-EnhancedPromptPath {
 
 	return $currentPath
 }
+
+
+# PSReadline customization
+# ========================
+# Set-PSReadlineKeyHandler -Key Tab -Function Complete # Unix completion
+Set-PSReadlineOption -BellStyle None # Remove the bell
+
+# Make sure to replace && with ; when copying git commands
+Set-PSReadlineKeyHandler -Key Ctrl+V `
+                         -BriefDescription SpecialGerritCopy `
+                         -LongDescription "Replace && with ; in git specific command line" `
+                         -ScriptBlock {
+    $text = Get-Clipboard
+
+    if ($text -ne $null) {
+        # check if it's a git command
+        if ($text -like "git*") {
+            [Microsoft.PowerShell.PSConsoleReadLine]::Insert($text.replace("&&", ";"))
+        } else {
+            [Microsoft.PowerShell.PSConsoleReadLine]::Insert($text)
+        }
+    }
+}
+
+
+# Modules
+# =======
+
+# Chocolatey profile
+#$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+#if (Test-Path($ChocolateyProfile)) {
+#  Import-Module "$ChocolateyProfile"
+#}
+
+# Posh-Alias module
+Import-Module posh-alias
+$AliasHash.GetEnumerator() | ForEach-Object{ Add-Alias $_.key $_.value }
+
+# Ps-Env
+Import-Module ps-env
+Set-PsEnvConfig $ToolsFile
+
+# Posh-Git
+Import-Module posh-git
+Start-SshAgent -Quiet
+
+# Cd-extra
+Import-Module cd-extras
+
+
+# Prompt
+# ======
 $GitPromptSettings.DefaultPromptPath = ''
 function prompt {
     # Your non-prompt logic here
